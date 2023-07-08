a478f7b54c9ff9dafa11b94fcb48ebd18938452a
jeyog <choijy8816@gmail.com>
2023-07-08 19:29:57 +0900

    feat: fortify 추가

---
+++ app/Actions/Fortify/CreateNewUser.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Actions\Fortify;
+
+use App\Models\User;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\Rule;
+use Illuminate\Validation\ValidationException;
+use Laravel\Fortify\Contracts\CreatesNewUsers;
+
+class CreateNewUser implements CreatesNewUsers
+{
+    use PasswordValidationRules;
+
+    /**
+     * Validate and create a newly registered user.
+     *
+     * @param array<string, string> $input
+     * @throws ValidationException
+     */
+    public function create(array $input): Response
+    {
+        $validator = Validator::make($input, [
+            'name' => ['required', 'string', 'max:255'],
+            'email' => [
+                'required',
+                'string',
+                'email',
+                'max:255',
+                Rule::unique(User::class),
+            ],
+            'password' => $this->passwordRules(),
+        ]);
+
+        if ($validator->fails()) {
+            throw new HttpResponseException(response()->json([
+                'success'   => false,
+                'message'   => 'Validation errors',
+                'data'      => $validator->errors()
+            ], 400));
+        }
+        $validator->validate();
+
+        return response(User::create([
+            'name' => $input['name'],
+            'email' => $input['email'],
+            'password' => Hash::make($input['password']),
+        ]), 201);
+    }
+}
---
+++ app/Actions/Fortify/CreateNewUserRequest.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Actions\Fortify;
+
+use App\Models\User;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Validation\Rule;
+
+class CreateNewUserRequest extends FormRequest
+{
+    use PasswordValidationRules;
+
+    public function rules(): array
+    {
+        return [
+            'name' => ['required', 'string', 'max:255'],
+            'email' => [
+                'required',
+                'string',
+                'email',
+                'max:255',
+                Rule::unique(User::class),
+            ],
+            'password' => $this->passwordRules(),
+        ];
+    }
+
+    public function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            'success'   => false,
+            'message'   => 'Validation errors',
+            'data'      => $validator->errors()
+        ], 400));
+    }
+
+    public function messages()
+    {
+        return [
+            'name.required' => 'name is required',
+            'email.required' => 'body is required',
+            'email.email' => 'invalid email',
+            'password.required',
+        ];
+    }
+}
